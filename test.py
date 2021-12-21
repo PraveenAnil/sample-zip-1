97ccad83eb6c64f116b8a47319769aca33fafa86
PraveenAnil <praveendosth@gmail.com>
2021-12-21 17:28:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,10 @@ chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options )
 driver.get("https://portal.azure.com")
+
+username = "ba"
+ 
+# find the id or name or class of
+# username by inspecting on username input
+driver.find_element_by_name("loginfmt").send_keys(username)
+   
