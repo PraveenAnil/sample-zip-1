6e13845d2516c5f771bd0a9119e90fcc1265cf66
PraveenAnil <praveendosth@gmail.com>
2021-12-21 19:21:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ chrome_options = Options()
 chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options )
+url = "replacel"
 driver.get("https://portal.azure.com")
 
-username = "replaceuser"
-password = "replacepass"
+username = "replaceus"
+password = "replacepa"
 # find the id or name or class of
 # username by inspecting on username input
 driver.find_element_by_name("loginfmt").send_keys(username)
