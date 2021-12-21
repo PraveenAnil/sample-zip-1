c42a129fa8d0ab06f449904594ff9cb157b711cd
PraveenAnil <praveendosth@gmail.com>
2021-12-21 14:09:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                          chrome_options=chrome_options
-                         )
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 driver.get("https://www.google.com")
 print(driver.title)
 driver.close()
