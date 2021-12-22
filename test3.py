da09d4d73bb159ecc3593af6b5143c8b2477592d
PraveenAnil <praveendosth@gmail.com>
2021-12-22 18:56:56 +0530

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+FIREFOXDRIVER_PATH = '/usr/local/bin/chromedriver'
+
+firefoxOptions = Options()
+firefoxOptions.add_argument('--start-maximized')
+driver = webdriver.Firefox(executable_path=FIREFOXDRIVER_PATH, options=firefoxOptions )
+driver.get("replacel")
+
+username = "replaceus"
+password = "replacepa"
+# find the id or name or class of
+# username by inspecting on username input
+driver.find_element_by_name("loginfmt").send_keys(username)
+driver.find_element_by_id("idSIButton9").click()
+
+delay = 10 # seconds
+myElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'i0118')))
+
+driver.find_element_by_name("passwd").send_keys(password)
+WebDriverWait(driver, delay)
+driver.find_element_by_id("idSIButton9").click()
